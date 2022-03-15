465439fcd07b29ff46d4e901748ae201195b70d4
Georgi172003 <g.yordanov2017@pmg-pernik.com>
2022-03-15 09:22:11 +0200

    Add Project With Admin

---
+++ Mechtan dom-12b 5/DreamHomeApp/DreamHomeApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DreamHomeApp.Entites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DreamHomeApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
